703f158a3afddfbb4439eb9f84612eb6dcade15c
Jane Pavlova <35457637+pavlovajane@users.noreply.github.com>
2023-05-03 12:37:31 -0400

    Complete 1-3 tasks

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, repetition) {
 
+    if (repetition<0) {
+        return "ERROR"
+    }
+
+    // starting cycle from 1 as we already have the string one time 
+    let repeat = ""
+  
+    for (let i = 0; i < repetition; i++) {
+        repeat += str;
+      }
+    return repeat;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(inputstr) {
+    
+    let len = inputstr.length;
+    let backwardstr = "";
+
+    for (let i = len-1; i >= 0; i--) {
+        backwardstr += inputstr[i];
+    }
+
+    return backwardstr;
 
 };
+
+// reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
