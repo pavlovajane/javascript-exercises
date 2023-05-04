473d8997620cc15460e9ec97869534dbc11fb895
Jane Pavlova <35457637+pavlovajane@users.noreply.github.com>
2023-05-04 11:07:30 -0400

    Add removeFromArray js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
 
+    // the first argument is always our target array
+    arr = arguments[0];
+
+    for (let i = 1; i < arguments.length; i++){
+        let index = arr.indexOf(arguments[i]);
+        if (index !== -1) {
+            arr.splice(index, 1);
+        }
+    }
+
+    return arr;
+    
 };
 
 // Do not edit below this line
