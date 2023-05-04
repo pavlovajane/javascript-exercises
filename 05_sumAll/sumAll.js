88a6578d8a8c2ec26c208fb70ffd3ad825cf5f05
Jane Pavlova <35457637+pavlovajane@users.noreply.github.com>
2023-05-04 11:32:06 -0400

    Sum all numbers js task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+
+    let sum = 0;
+
+    if (!Number.isInteger(first) || !Number.isInteger(second) || first < 0 || second < 0){
+        return 'ERROR'
+    }
+
+    let min = Math.min(first, second);
+    let max = Math.max(first, second);
+
+    for (min; min <= max; min++) {
+        sum += min; 
+    }
+
+    return sum;
 
 };
 
