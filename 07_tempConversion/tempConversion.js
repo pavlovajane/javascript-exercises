f83ca0bcbcb4eb00203e22920da56579e0b450f4
Jane Pavlova <35457637+pavlovajane@users.noreply.github.com>
2023-05-05 16:15:30 -0400

    Add temp conversion task solution js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  return round((f - 32)*5/9, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return round((c * 9/5) + 32, 1);
 };
 
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+// console.log(convertToCelsius(32));
+// console.log(convertToFahrenheit(0));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
