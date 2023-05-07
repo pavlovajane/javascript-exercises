69506683c2b26bc4c438b764f040f9f9f6cccfb3
Jane Pavlova <35457637+pavlovajane@users.noreply.github.com>
2023-05-07 16:35:26 -0400

    Add calculator.js solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return Number(a+b)
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return Number(a-b);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++ ) {
+    sum += array[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  
+  let mult;
+  if (array.length == 1) {
+    mult = array[0];
+  }
+  else if (array.length == 0) {
+    return "ERROR"
+  }
+  else {
+    mult = 1;
+  };
 
+  for (let i = 0; i < array.length; i++ ) {
+    mult *= array[i];
+  }
+
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a == 0){
+    return 1;
+  }
+  else {
+    return (a*factorial(a-1));
+  }
+  
 };
 
 // Do not edit below this line
