a5e68fae7c5d83158db56995cbf3ab39de85ebac
Jane Pavlova <35457637+pavlovajane@users.noreply.github.com>
2023-05-08 10:23:12 -0400

    Add Fibonacci with recursion js solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = Number(number);
+    if (number < 0) {
+        return "OOPS";
+    }
+    if (number < 2) {
+        // basic case
+        return number;
+    }
 
+    return fibonacci(number-1) + fibonacci(number-2);
 };
+
+console.log(fibonacci(4));
+console.log(fibonacci(-25));
 
 // Do not edit below this line
 module.exports = fibonacci;
