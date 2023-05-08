a9fa406d0a346570961571663c93e1d0a41cfa1d
Jane Pavlova <35457637+pavlovajane@users.noreply.github.com>
2023-05-08 14:08:33 -0400

    Add findTheOldest using Array.reduce task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,49 @@
-const findTheOldest = function() {
+const people = [
+    {
+        name: "Carly",
+        yearOfBirth: 2018,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+]
 
+function getAge(person) {
+
+    if ("yearOfDeath" in person)
+        yearOfDeath = person.yearOfDeath;
+    else
+        yearOfDeath = new Date(Date.now()).getFullYear();
+        
+
+    yearOfBirth = person.yearOfBirth;
+
+    return Number(yearOfDeath - yearOfBirth)
+}
+
+const findTheOldest = function(people) {
+    
+    initialValue = people[0];
+    return people.reduce(
+        (accumulator, currentValue) => {
+
+            agePrev = getAge(accumulator);
+            ageCurrent = getAge(currentValue);
+
+            if (ageCurrent > agePrev)
+                return currentValue;
+            return accumulator
+
+        }, initialValue
+    );
 };
-
+console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
