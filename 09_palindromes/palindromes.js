d319d35f0b3b5fcfb3702ec9e3c6dea38c4a3de2
Jane Pavlova <35457637+pavlovajane@users.noreply.github.com>
2023-05-08 10:10:02 -0400

    Add palindromes task solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function(string) {
+    // determine only latin alphabet polindromes
+    // remove all non-letter symbols
+    let str = string.replace(/[^a-z0-9]/gi, "");
+    str = str.toLowerCase();
+    let len = str.length;
+    let i = 0;
+    let j = len-1;
 
+    do {
+        if (str[i] !== str[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    } while (i < j);
+    return true;
 };
 
 // Do not edit below this line
